b512bcfecb8b1a9530614f1209c34c2d7012e7a2
Ashish <ashish.cloudwapp@gmail.com>
2026-04-15 19:18:27 +0530

    update db js file

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
 import env from './env.js';
 
+console.log("DB_HOST:", env.DB_HOST);
+console.log("DB_USER:", env.DB_USER);
+console.log("DB_NAME:", env.DB_NAME);
+
 const sequelize = new Sequelize(
   env.DB_NAME,
   env.DB_USER,
